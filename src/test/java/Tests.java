b72bf8f4092b35e12615882802bbed697d06ca32
Vasiliy <vsmaksimov@inbox.ru>
2020-02-13 16:47:59 +0300

    delete tests and edit test class

--- src/test/java/Tests.java
+++ src/test/java/Tests.java
@@ -4,36 +4,14 @@ import org.junit.Test;
 import static org.hamcrest.Matchers.equalTo;
 
 public class Tests {
-    @Test
-    public void testId() {
-        String url = "https://jsonplaceholder.typicode.com/todos/1";
-        RestAssured.
-                get(url)
-                .then()
-                .body("userId", equalTo(1));
-    }
+    private final String baseUri = "https://reqres.in/";
+    private final String basePathSingleUser = "api/users/2";
 
     @Test
-    public void testPrintResponse() {
-        String baseUri = "https://jsonplaceholder.typicode.com";
-        String basePath = "todos/1";
-
-        RestAssured.given()
+    public void testSingleUserJsonStructure() {
+              RestAssured.given()
                 .baseUri(baseUri)
-                .basePath(basePath)
-                .when().get()
-                .then().extract().response()
-                .prettyPrint();
-    }
-
-    @Test
-    public void testJsonApi() {
-        String baseUri = "https://reqres.in/";
-        String basePath = "api/users/2";
-
-        RestAssured.given()
-                .baseUri(baseUri)
-                .basePath(basePath)
+                .basePath(basePathSingleUser)
                 .when().get()
                 .then()
                 .assertThat()
